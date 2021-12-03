2af8895c6bd1cd169d4ae622fc7b469f50ad74b8
AndrushaPixeL <surnovandrey@gmail.com>
2021-12-03 05:35:31 +0300

    feat: refactor, header

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import React from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
 
-function Link(props) {
-  return (
-    <Text
-      {...props}
-      accessibilityRole="link"
-      style={StyleSheet.compose(styles.link, props.style)}
-    />
-  );
-}
-
-function App() {
+const App = () => {
   return (
     <View style={styles.app}>
-      <View style={styles.header}>
-        <Image
-          accessibilityLabel="React logo"
-          resizeMode="contain"
-          style={styles.logo}
-        />
-        <Text style={styles.title}>React Native for Web</Text>
-      </View>
-      <Text style={styles.text}>
-        This is an example of an app built with
-        <Link href="https://github.com/facebook/create-react-app">
-          Create React App
-        </Link>
-        and
-        <Link href="https://github.com/necolas/react-native-web">
-          React Native for Web
-        </Link>
-      </Text>
-      <Text style={styles.text}>
-        To get started, edit
-        <Link href="https://codesandbox.io/s/q4qymyp2l6/" style={styles.code}>
-          src/App.js
-        </Link>
-        .
-      </Text>
-      <Button onPress={() => {}} title="Example button" />
+     <Header />
     </View>
   );
 }
@@ -47,31 +13,7 @@ function App() {
 const styles = StyleSheet.create({
   app: {
     marginHorizontal: "auto",
-    maxWidth: 500
-  },
-  logo: {
-    height: 80
-  },
-  header: {
-    padding: 20
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: "1.5rem",
-    marginVertical: "1em",
-    textAlign: "center"
-  },
-  text: {
-    lineHeight: "1.5em",
-    fontSize: "1.125rem",
-    marginVertical: "1em",
-    textAlign: "center"
-  },
-  link: {
-    color: "#1B95E0"
-  },
-  code: {
-    fontFamily: "monospace, monospace"
+    width: "100%"
   }
 });
 
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+const Header = () => {
+  return <View style={styles.header}>
+      <Text style={styles.title}> TEST</Text>
+  </View>
+}
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 50,
+    backgroundColor: "rgb(87, 189, 255)",
+    display: "flex",
+    justifyContent: "center",
+    padding: 16
+  },
+  title: {
+    fontWeight: "normal",
+    fontSize: "1.5rem",
+    marginVertical: "1em",
+    textAlign: "start"
+  },
+})
+
+export default Header
