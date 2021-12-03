5f8e91536835efd7ac0f8aeddcd5017f752fe019
AndrushaPixeL <surnovandrey@gmail.com>
2021-12-03 05:10:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+
+function Link(props) {
+  return (
+    <Text
+      {...props}
+      accessibilityRole="link"
+      style={StyleSheet.compose(styles.link, props.style)}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={styles.app}>
+      <View style={styles.header}>
+        <Image
+          accessibilityLabel="React logo"
+          resizeMode="contain"
+          style={styles.logo}
+        />
+        <Text style={styles.title}>React Native for Web</Text>
+      </View>
+      <Text style={styles.text}>
+        This is an example of an app built with
+        <Link href="https://github.com/facebook/create-react-app">
+          Create React App
+        </Link>
+        and
+        <Link href="https://github.com/necolas/react-native-web">
+          React Native for Web
+        </Link>
+      </Text>
+      <Text style={styles.text}>
+        To get started, edit
+        <Link href="https://codesandbox.io/s/q4qymyp2l6/" style={styles.code}>
+          src/App.js
+        </Link>
+        .
+      </Text>
+      <Button onPress={() => {}} title="Example button" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  app: {
+    marginHorizontal: "auto",
+    maxWidth: 500
+  },
+  logo: {
+    height: 80
+  },
+  header: {
+    padding: 20
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  text: {
+    lineHeight: "1.5em",
+    fontSize: "1.125rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  link: {
+    color: "#1B95E0"
+  },
+  code: {
+    fontFamily: "monospace, monospace"
+  }
+});
 
 export default App;
